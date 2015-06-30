8d44df908edcf4a7ae97d8186285b25e93453196
Anand Muley <Anand.Muley@cognizant.com>
2015-06-30 19:20:53 +0530

    Creating Test Cases for Controllers.

--- WebContent/karma-config.js
+++ WebContent/karma-config.js
@@ -4,6 +4,8 @@ module.exports = function(config){
 		files : [
 		         'node_modules/angular/angular.js',
 		         'node_modules/angular-mocks/angular-mocks.js',
+		         'node_modules/angular-route/angular-route.js',
+		         'scripts/**/*.js',
 		         'test/**/*Test.js'
 		         ],
 		autoWatch : true,
---
+++ WebContent/test/controllers/IndexControllerTest.js
@@ -0,0 +1,30 @@
+describe('IndexControllerTest',function(){
+	
+	var indexController,$scope;
+	
+	// Setup
+	beforeEach(module('ICareUI'));
+	beforeEach(inject(function($injector,$controller){
+		$scope = {};
+		
+		createController = function(){
+			$controller('IndexController',{
+				'$scope':$scope
+			});
+		}
+		
+		createController();
+		
+	}));
+	
+	it('Should test for the page title',function(){
+		// GIVEN
+		
+		// WHEN
+		
+		// THEN
+		expect($scope.pageTitle).toBe('Index Page');
+		
+	});
+	
+});

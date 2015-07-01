6482bc24cf915f36280c8e8e5b0fe5a230a87a4d
Anand Muley <andriox20@gmail.com>
2015-07-01 23:27:40 +0530

    Testing Controllers Done

--- WebContent/test/controllers/AccessControllerTest.js
+++ WebContent/test/controllers/AccessControllerTest.js
@@ -1,15 +1,17 @@
 describe('AccessControllerTest',function(){
 
-	var $scope,loginService;
+	var $scope,loginService,$location;
 	
 	beforeEach(module('ICareUI'));
 	beforeEach(inject(function($injector,$controller,LoginService){
 		$scope = $injector.get('$rootScope');
 		loginService = LoginService;
+		$location = $injector.get('$location');
 		createController = function(){
 			return $controller('LoginController',{
 				'$scope':$scope,
-				'LoginService' : LoginService
+				'LoginService' : LoginService,
+				'$location' : $location
 			});
 		};
 		
@@ -23,12 +25,27 @@ describe('AccessControllerTest',function(){
 	
 	it('Should authenticate login',function(){
 		// GIVEN
+		$scope.username = 'a';
+		$scope.password = 'a';
 		
 		// WHEN
-		$scope.authenticate()
+		$scope.authenticate();
 		
 		// THEN
+		expect($location.path()).toBe('/home');
 		
+	});
+	
+	it('Should not authenticate login',function(){
+		// GIVEN
+		$scope.username = 'a';
+		$scope.password = 'b';
+		
+		// WHEN
+		$scope.authenticate();
+		
+		// THEN
+		expect($location.path()).toBe('');
 	});
 	
 });

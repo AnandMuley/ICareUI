c897bf107e05488bda64b94b6da6c021b008a94c
Anand Muley <Anand.Muley@cognizant.com>
2015-08-20 10:19:39 +0530

    Added Coverage Reporter to Project. Uncomment the code for enabling the coverage reporter.

--- src/test/karma-config.js
+++ src/test/karma-config.js
@@ -1,24 +1,21 @@
-module.exports = function(config){
+module.exports = function(config) {
 	config.set({
-		frameworks : ['jasmine'],
-		files : [
-		         'node_modules/jquery/dist/jquery.min.js',
-		         'node_modules/angular/angular.js',
-		         'node_modules/angular-mocks/angular-mocks.js',
-		         'node_modules/angular-route/angular-route.js',
-		         '../main/webapp/scripts/**/*.js',
-		         'controllers/*.js',
-		         'services/*.js',
-		         'dataproviders/*.js'
-		         ],
-//		         preprocessors : {
-//		        	 'scripts/**/*.js' : ['coverage']
-//		         },
-//		         reporters : ['coverage'],
-		         reporters : ['spec'],
-		         autoWatch : true,
-		browsers : ['PhantomJS'],
-		plugins : ['karma-jasmine','karma-phantomjs-launcher','karma-coverage','karma-spec-reporter'],
+		frameworks : [ 'jasmine' ],
+		files : [ 'node_modules/jquery/dist/jquery.min.js',
+				'node_modules/angular/angular.js',
+				'node_modules/angular-mocks/angular-mocks.js',
+				'node_modules/angular-route/angular-route.js',
+				'../main/webapp/scripts/**/*.js', 'controllers/*.js',
+				'services/*.js', 'dataproviders/*.js' ],
+//		preprocessors : {
+//			'../main/webapp/scripts/**/*.js' : 'coverage'
+//		},
+//		reporters : [ 'coverage', 'spec' ],
+		reporters : ['spec'],
+		autoWatch : true,
+		browsers : [ 'PhantomJS' ],
+		plugins : [ 'karma-jasmine', 'karma-phantomjs-launcher',
+				'karma-coverage', 'karma-spec-reporter' ],
 		colors : true
 	});
 }
